a6bc934741cc20f73fd814f2ef69800f8fdce328
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-11-28 14:28:27 -0500

    bunch of asthetic changes

--- Lab21/Scripts/MYJAVASTUFF.js
+++ Lab21/Scripts/MYJAVASTUFF.js
@@ -1,42 +1,83 @@
 ﻿function validate() {
-
+    var flag = true;
+    //first name
     if (document.theForm.firstName.value == "") {
-        alert("Please provide your first name!");
         document.theForm.firstName.focus();
-        return false;
+        document.theForm.firstName.setAttribute("class", "form-control is-invalid");
+        document.getElementById("firstNameValidate").setAttribute("class", "invalid-feedback");
+        document.getElementById("firstNameValidate").innerHTML = "Sorry, that first name is terrible. Try another?";
+        flag = false;
     }
-
+    else {
+        document.theForm.firstName.setAttribute("class", "form-control is-valid");
+        document.getElementById("firstNameValidate").setAttribute("class", "valid-feedback");
+        document.getElementById("firstNameValidate").innerHTML = "Successful!";
+    }
+    //last name
     if (document.theForm.lastName.value == "") {
-        alert("Please provide your last name!");
         document.theForm.lastName.focus();
-        return false;
+        document.theForm.lastName.setAttribute("class", "form-control is-invalid");
+        document.getElementById("lastNameValidate").setAttribute("class", "invalid-feedback");
+        document.getElementById("lastNameValidate").innerHTML = "Sorry, ive never heard of 'blank'. Try a little harder?";
+        flag = false;
+    }
+    else {
+        document.theForm.lastName.setAttribute("class", "form-control is-valid");
+        document.getElementById("lastNameValidate").setAttribute("class", "valid-feedback");
+        document.getElementById("lastNameValidate").innerHTML = "Successful!";
     }
 
     if (document.theForm.email.value == "") {
-        alert("Please provide your Email!");
         document.theForm.email.focus();
-        return false;
+        document.theForm.email.setAttribute("class", "form-control is-invalid");
+        document.getElementById("emailNameValidate").setAttribute("class", "invalid-feedback");
+        document.getElementById("emailNameValidate").innerHTML = "Do i even have to say why...?";
+        flag = false;
+    } else {
+        document.theForm.email.setAttribute("class", "form-control is-valid");
+        document.getElementById("emailNameValidate").setAttribute("class", "valid-feedback");
+        document.getElementById("emailNameValidate").innerHTML = "Successful!";
     }
+
+
     var tempPhone = document.theForm.phone.value;
     if (tempPhone.length != 10)
     {
-        alert("Phone# sucks");
         document.theForm.phone.focus();
-        return false;
+        document.theForm.phone.setAttribute("class", "form-control is-invalid");
+        document.getElementById("phoneNameValidate").setAttribute("class", "invalid-feedback");
+        document.getElementById("phoneNameValidate").innerHTML = "I t S  j U s T  1 0  D i G i T s";
+        flag = false;
+    } else {
+        document.theForm.phone.setAttribute("class", "form-control is-valid");
+        document.getElementById("phoneNameValidate").setAttribute("class", "valid-feedback");
+        document.getElementById("phoneNameValidate").innerHTML = "Successful!";
     }
 
     if (document.theForm.pass.value == "") {
-        alert("need that pass...");
         document.theForm.pass.focus();
-        return false;
-    }
-    if (document.theForm.pass2.value != document.theForm.pass.value) {
-        alert("Passwords dont match");
-        document.theForm.pass2.focus();
-        return false;
+        document.theForm.pass.setAttribute("class", "form-control is-invalid");
+        document.getElementById("passNameValidate").setAttribute("class", "invalid-feedback");
+        document.getElementById("passNameValidate").innerHTML = "Hey everyone there password is !";
+        flag = false;
+    } else {
+        document.theForm.pass.setAttribute("class", "form-control is-valid");
+        document.getElementById("passNameValidate").setAttribute("class", "valid-feedback");
+        document.getElementById("passNameValidate").innerHTML = "Look at you go!";
     }
 
-    return (true);
+    if (document.theForm.pass2.value != document.theForm.pass.value || document.theForm.pass2.value == "") {
+        document.theForm.pass2.focus();
+        document.theForm.pass2.setAttribute("class", "form-control is-invalid");
+        document.getElementById("pass2NameValidate").setAttribute("class", "invalid-feedback");
+        document.getElementById("pass2NameValidate").innerHTML = "holding your hand at this point";
+        flag = false;
+    } else {
+        document.theForm.pass2.setAttribute("class", "form-control is-valid");
+        document.getElementById("pass2NameValidate").setAttribute("class", "valid-feedback");
+        document.getElementById("pass2NameValidate").innerHTML = "Miracles DO happen!";
+    }
+        return (flag);
 }
 
 function test() {
