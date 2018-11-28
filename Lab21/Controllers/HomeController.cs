a41b118dbdf5136d8e8560636ed0e02858fdebfa
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-11-28 09:17:49 -0500

    Add project files.

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult registerComplete(string firstName, string lastName, string email, string phone, string pass)
+        {
+            ViewBag.fn = firstName;
+            return View();
+        }
+    }
+}
---
+++ Lab21/Scripts/MYJAVASTUFF.js
@@ -0,0 +1,38 @@
+﻿function validate() {
+
+    if (document.theForm.firstName.value == "") {
+        alert("Please provide your first name!");
+        document.theForm.firstName.focus();
+        return false;
+    }
+
+    if (document.theForm.lastName.value == "") {
+        alert("Please provide your last name!");
+        document.theForm.lastName.focus();
+        return false;
+    }
+
+    if (document.theForm.email.value == "") {
+        alert("Please provide your Email!");
+        document.theForm.email.focus();
+        return false;
+    }
+    var tempPhone = document.theForm.phone.value;
+    if (tempPhone.length != 10)
+    {
+        alert("Phone# sucks");
+        document.theForm.phone.focus();
+        return false;
+    }
+
+    if (document.theForm.pass.value == "") {
+        alert("need that pass...");
+        document.theForm.pass.focus();
+        return false;
+    }
+    return (true);
+}
+
+function test() {
+    document.getElementsByTagName("h2")[0].innerHTML = "test";
+}
